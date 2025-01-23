7aeb478803a722d3dca00a323e3ed7117f859e40
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-23 12:40:37 +0545

    while loop

---
+++ WhileLoop.js
@@ -0,0 +1,27 @@
+// while loop is a loop that is used to iterate over a block of code as long as a specified condition is true.
+// it is also considered as a entry controlled loop.
+// Note: we have to initialize the variable before using while loop and should increment or decrement the variable inside the while loop.
+
+// Syntax: while(condition){ //code to be executed }
+
+// Example:
+
+let i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
