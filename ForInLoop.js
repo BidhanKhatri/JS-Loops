69e683b5b3168ca9097036d1b4a0a0c2b17e38ab
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-23 12:51:07 +0545

    for in loop

---
+++ ForInLoop.js
@@ -0,0 +1,18 @@
+// for in loop is used with objects.
+// it is used to iterate over the properties of an object.
+// we can access the key and value of objects using for in loop
+
+// Syntax: for (let key in object){ //code to be executed }
+
+// Example:
+
+const person = {
+  name: "Ram",
+  age: 25,
+};
+
+for (let keys in person) {
+  //console.log(keys);        // name, age
+  //console.log(person[keys]);    // Ram, 25
+  console.log(keys, person[keys]); // name Ram, age 25
+}
