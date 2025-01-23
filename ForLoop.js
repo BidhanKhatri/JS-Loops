18a01a170a819f7eeb4f60b18c8c2b5dbb61415c
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-23 12:35:40 +0545

    for() loop

---
+++ ForLoop.js
@@ -0,0 +1,13 @@
+// for() loop is a loop that is used to iterate over a block of code a specified number of times.
+// use case senario, if we want to print hello world 100 time, we can use for loop
+
+// Syntax: for(initialization; condition; increment/decrement){ //code to be executed }
+
+// Example:
+
+for (let i = 0; i < 100; i++) {
+  console.log("Hello world!");
+}
+
+// Output:
+// Hello world! X 100
